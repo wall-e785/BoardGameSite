def34ef1c8f5e20281c7a3c5dfc49b7423a726c2
jaspergass <115849451+jaspergass@users.noreply.github.com>
2025-03-29 18:14:22 -0700

    image gallery

--- index.php
+++ index.php
@@ -6,24 +6,69 @@
         include("./private/index-script.php");
     ?>
     <div class="body">
-    
-    
-        <div class="flex row">
-            <img class="playtested-icon" src="./imgs/playtested.svg">
-            <div>
-                <h1>Playtested.</h1>
-                <h3>By real people, like you. </h3>
-            </div> 
-        </div>
+
+        <?php
+        // Check if session exists 
+            if(!isset($_SESSION['username'])){
+                // not logged in, display seciton to sign up
+                echo "<div class=\"index-header-no-user\">";
+                    echo "<div class=\"border-right\">";
+                        echo "<div class=\"index-title\">";
+                            echo "<img class=\"playtested-icon\" src=\"./imgs/playtested.svg\">";
+                            echo "<div>";
+                                echo "<h1>Playtested.</h1>";
+                                echo "<h3>By real people, like you. </h3>";
+                            echo "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "<div class=\"index-header-signup\">";
+                        echo "<h3>Join the community</h3>";
+                        echo "<p>You must be logged in to leave comments and make collections of your favourite games.</p>";
+                    echo "</div>";
+                echo "</div>";
+            }else{
+                // user is logged in, so just display the title
+                echo "<div class=\"index-title index-header-signedin\">";
+                    echo "<img class=\"playtested-icon\" src=\"./imgs/playtested.svg\">";
+                    echo "<div>";
+                        echo "<h1>Playtested.</h1>";
+                        echo "<h3>By real people, like you. </h3>";
+                    echo "</div>";
+                echo "</div>";
+            }
+        ?>
+        
         <div>
             <h2>Top Ranked Games</h2>
-            <div class="flex row">
+            <div class="">
+                <p>See a list of all games in order of highest rank</p>
+                <img src="./imgs/arrow-right.svg">
+            <div>
+            <div class="index-img-scroll-container">
                 <?php
                     while ($row = $res->fetch_assoc()) {
-                        echo "<div>";
-                            echo "<img src=\"".$row['image_url']."\">";
-                            echo "<h3>".$row['avg_rating']."</h3>";
-                            echo "<p>".$row['names']."</p>";
+                        echo "<div class=\"index-gallery-item\">";
+                            echo "<img class=\"index-gallery-img\" src=\"".$row['image_url']."\">";
+                            echo "<div class=\"index-gallery-text\">";
+                                echo "<h3>".round($row['avg_rating'], 2)."</h3>";
+                                echo "<p>".$row['names']."</p>";
+                            echo "</div>";
+                        echo "</div>";
+                    }
+                ?>
+            </div>
+        </div>
+        <div>
+            <h2>Best Two Player Games</h2>
+            <div class="index-img-scroll-container">
+                <?php
+                    while ($row = $res2->fetch_assoc()) {
+                        echo "<div class=\"index-gallery-item\">";
+                            echo "<img class=\"index-gallery-img\" src=\"".$row['image_url']."\">";
+                            echo "<div class=\"index-gallery-text\">";
+                                echo "<h3>".round($row['avg_rating'], 2)."</h3>";
+                                echo "<p>".$row['names']."</p>";
+                            echo "</div>";
                         echo "</div>";
                     }
                 ?>
