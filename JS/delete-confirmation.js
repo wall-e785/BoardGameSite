443407d425f5b6e5a908cae1453b37b013a7676a
wall-e785 <wall_echau604@yahoo.ca>
2025-04-07 14:10:29 -0700

    Comments deleted

--- JS/delete-confirmation.js
+++ JS/delete-confirmation.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
-    var delete_button = document.getElementById("delete");
+    var delete_button_id = document.getElementById("delete");
+    var delete_button_class = document.getElementsByClassName("comment");
 
-    if(delete_button){
-        delete_button.addEventListener('click', function(){
+    if(delete_button_id){
+        delete_button_id.addEventListener('click', function(){
             //referenced confirmation from: https://www.w3schools.com/js/js_popup.asp
-            if(confirm("Are you sure you want to delete this collection?")){
-                //redirect referenced from: w3schools.com/howto/howto_js_redirect_webpage.asp
-                window.location.href = delete_button.getAttribute("data-url");
+            if(delete_button_id.className == "collection"){
+                if(confirm("Are you sure you want to delete this collection?")){
+                    //redirect referenced from: w3schools.com/howto/howto_js_redirect_webpage.asp
+                    window.location.href = delete_button.getAttribute("data-url");
+                }
             }
             // 
         });
-    }
+    }else if(delete_button_class){
+        for(var i=0; i<delete_button_class.length; i++){
+            delete_button_class[i].addEventListener("click", deleteComment);
+        }
+    }   
+
+    function deleteComment(){
+        if(confirm("Are you sure you want to delete this comment?")){
+            //redirect referenced from: w3schools.com/howto/howto_js_redirect_webpage.asp
+            console.log(this);
+            window.location.href = this.getAttribute("data-url");
+        }
+    };
 });
