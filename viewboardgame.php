62b6497f9f3465d25f92bc063a1f800fc975d6e1
jaspergass <115849451+jaspergass@users.noreply.github.com>
2025-04-08 20:07:54 -0700

    ratings fix

--- viewboardgame.php
+++ viewboardgame.php
@@ -68,10 +68,8 @@
                 $new_sum += floatval($row['rating_num']);    
             }
             $avg_rating = round(($existing_avg * $num_votes + $new_sum) / ($num_votes + $new_count), 5);
-        }else{
-            // there are no new ratings for this game, so display the existing avg.
-            $avg_rating = $existing_avg;
         }
+       
         // Free the result 
         $res->free_result();
 
